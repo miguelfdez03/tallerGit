d4a58c2eaa9a8719ac6bdc723386f02163ad45f4
itjimene <itjimene@student.42malaga.com>
2024-12-12 17:09:18 +0100

    Work done

--- minishell.c
+++ minishell.c
@@ -3,14 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miguel-f <miguel-f@student.42.fr>          +#+  +:+       +#+        */
+/*   By: itjimene <itjimene@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 17:00:04 by miguel-f          #+#    #+#             */
-/*   Updated: 2024/12/12 17:00:23 by miguel-f         ###   ########.fr       */
+/*   Updated: 2024/12/12 17:07:45 by itjimene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdio.h"
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
 
 void main()
 {
