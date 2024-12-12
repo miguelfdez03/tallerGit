001942c0e1981032858f3f9326cbb5e63c938e53
Miguel Angel Fernandez Munoz <miguel-f@c1r4s2.42malaga.com>
2024-12-12 17:05:37 +0100

    putstr

--- minishell.c
+++ minishell.c
@@ -6,13 +6,27 @@
 /*   By: miguel-f <miguel-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 17:00:04 by miguel-f          #+#    #+#             */
-/*   Updated: 2024/12/12 17:00:23 by miguel-f         ###   ########.fr       */
+/*   Updated: 2024/12/12 17:05:05 by miguel-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdio.h"
 
+int ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (0);
+}
+
 void main()
 {
-	printf("Hello World\n");
+	char *str = "Hello World";
+	printf("string: %s\n", str);
+	printf("string length: %d\n", ft_putstr(str));
 }
